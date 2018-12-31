67fd40dfff04b99b2c187814522935e6f75ace68
farkhodsadykov <sadykovfarkhod@gmail.com>
2018-12-31 01:38:46 -0600

    Fixed root folder

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,9 @@ MAINTAINER Farkhod Sadykov
 
 # Install Jupyter Notebook
 RUN python3.6 -m pip install jupyter
+RUN python3.6 -m pip install yaml
+RUN mkdir /root/.jupyter/ -p
 
 # Chance logo jupyter notebook
 COPY logo.png /usr/lib/python3.6/site-packages/notebook/static/base/images/logo.png
-
-
-# Run Jupyter NoteBook when container starts
-ENTRYPOINT ["/usr/bin/jupyter"]
-CMD ["notebook", "--ip=0.0.0.0",  "--port=8888", "--no-browser", "--allow-root"]
+COPY jupyter-runner.py /
--- jupyter-runner.py
+++ jupyter-runner.py
@@ -7,16 +7,19 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description='Jupyter notebook runner')
 
+    parser.add_argument('-u', '--username',  help='Username for jupyter notebook')
     parser.add_argument('-p', '--password',  help='Password for jupyter notebook')
 
     args = parser.parse_args()
 
     data = {'NotebookApp': {'password': str(args.password) }}
     # For mac if you
-    with open('/Users/fsadykov/.jupyter/jupyter_notebook_config.json', 'w') as file:
-    # with open('/root/.jupyter/jupyter_notebook_config.json', 'w') as file:
+    with open('/root/.jupyter/jupyter_notebook_config.json', 'w') as file:
         json.dump(data, file, indent=2)
 
+    os.system(f"mkdir /{args.username.lower()}")
+    os.system(f"cd /{args.username}")
     os.system('jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root')
+    
 if __name__ == '__main__':
     main()
