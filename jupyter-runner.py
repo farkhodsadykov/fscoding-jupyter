27af7054cfbf4a049e66b4f99dea7e834942d9af
farkhodsadykov <sadykovfarkhod@gmail.com>
2018-12-31 00:58:29 -0600

    New future has been added to the Dockerfile

--- jupyter-runner.py
+++ jupyter-runner.py
@@ -1,18 +1,22 @@
+import json, yaml
 import os
+
 
 import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Jupyter notebook runner')
 
-    parser.add_argument('-u', '--username', help='Username for jupyter notebook')
-
     parser.add_argument('-p', '--password',  help='Password for jupyter notebook')
 
     args = parser.parse_args()
-    print('This arg has been given :' + args.username + ' ' + args.password)
 
+    data = {'NotebookApp': {'password': str(args.password) }}
+    # For mac if you
+    with open('/Users/fsadykov/.jupyter/jupyter_notebook_config.json', 'w') as file:
+    # with open('/root/.jupyter/jupyter_notebook_config.json', 'w') as file:
+        json.dump(data, file, indent=2)
 
-
+    os.system('jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root')
 if __name__ == '__main__':
     main()
